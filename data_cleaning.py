59ea0c9a0f1386cdcfc71ad2fcedd19188543b99
André Grant <andre.b.grant@gmail.com>
2023-12-01 02:42:36 +0000

    Reformat code - Thanks Ruff!

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,36 +1,38 @@
 import pandas as pd
-#from dateutil.parser import parse
+# from dateutil.parser import parse
 
 
 class DataCleaning:
     def __init__(self) -> None:
         pass
-    
+
     def clean_unkown_string(self, df):
-        mask = r'^[A-Z0-9]{10}$'
+        mask = r"^[A-Z0-9]{10}$"
         for column in df.columns:
             df[column] = df[column][~df[column].astype(str).str.contains(mask, na=False)]
     
     def clean_dates(self, df):
         for column in df.columns:
-            if 'date' in column:
-                df[column] = pd.to_datetime(df[column], errors='coerce', format='mixed')
+            if "date" in column:
+                df[column] = pd.to_datetime(df[column], errors="coerce", format="mixed")
                 df.dropna(subset=column, inplace=True)
 
     def clean_address(self, df):
-        df.dropna(subset='address', inplace=True)
+        df.dropna(subset="address", inplace=True)
+
         def clean_single_address(address):
-            return address.replace('\n', ', ') if '\n' in address else address
-        df['address'] = df['address'].apply(clean_single_address) 
-        
+            return address.replace("\n", ", ") if "\n" in address else address
+
+        df["address"] = df["address"].apply(clean_single_address)
+
     def reset_index_col(self, df, index_col):
         if index_col != None:
             df.set_index(index_col, inplace=True, drop=True)
             df.reset_index(drop=True, inplace=True)
             df.index = df.index + 1
         else:
-            return df   
-        
+            return df
+
     def clean_user_data(self, df, index_col=None):
         self.clean_dates(df)
         self.clean_unkown_string(df)
@@ -41,25 +43,32 @@ class DataCleaning:
     #     mask = ((df['longitude'] == 'N/A') | df['longitude'].isna()) | \
     #         ((df['lat'] == 'N/A') | df['lat'].isna())
     #     df.loc[mask, ['longitude', 'lat']] = 'N/A'
-          
+
     @staticmethod
     def clean_card_data(df):
         df['expiry_date'] = pd.to_datetime(df['expiry_date'], errors='coerce', format='%m/%y')
         df['expiry_date'] = df['expiry_date'].dt.strftime('%m/%y')
         df['date_payment_confirmed'] = pd.to_datetime(df['date_payment_confirmed'], errors='coerce')
         df.dropna(inplace=True)
-    
+
     def clean_store_data(self, df):
         self.clean_unkown_string(df)
         self.clean_address(df)
         self.clean_dates(df)
-        #self.clean_coordinates(df)
-        #self.reindex_store_columns(df)
-        self.df = df.reindex(columns=['index', 'store_code', 'store_type', 'staff_numbers', 'address', 'longitude',	'latitude', 'locality', 'country_code', 'continent', 'opening_date'])
-
-
-
-        
-
-
-
+        # self.clean_coordinates(df)
+        # self.reindex_store_columns(df)
+        self.df = df.reindex(
+            columns=[
+                "index",
+                "store_code",
+                "store_type",
+                "staff_numbers",
+                "address",
+                "longitude",
+                "latitude",
+                "locality",
+                "country_code",
+                "continent",
+                "opening_date",
+            ]
+        )
--- data_extraction.py
+++ data_extraction.py
@@ -1,9 +1,6 @@
 from database_utils import DatabaseConnector
 from data_cleaning import DataCleaning
 import pandas as pd
-import data_cleaning as dc
-import tabula
-import requests
 import tabula
 import requests
 
@@ -14,9 +11,9 @@ class DataExtractor:
         # self.creds = self.connector.read_db_creds()
         # self.engine = self.connector.init_db_engine()
         pass
-    
+
     @staticmethod
-    def read_rds_table(instance, table, creds_yaml): 
+    def read_rds_table(instance, table, creds_yaml):
         creds = instance.read_db_creds(creds_yaml)
         engine = instance.init_db_engine(creds)
         with engine.connect() as conn:
@@ -25,21 +22,21 @@ class DataExtractor:
 
     @staticmethod
     def retrieve_pdf_data(url):
-        df = tabula.read_pdf(url, 'dataframe', pages='all', multiple_tables=False)
+        df = tabula.read_pdf(url, "dataframe", pages="all", multiple_tables=False)
         return df[0]
-    
+
     @staticmethod
     def list_number_of_stores(url, headers):
         response = requests.get(url, headers=headers)
         if response.status_code == 200:
             data = response.json()
-            number_of_stores = data['number_stores']
+            number_of_stores = data["number_stores"]
             print(f"Number of stores: {data['number_stores']}")
         return number_of_stores
-    
+
     @staticmethod
     def retrieve_stores_data(url, headers):
-        store_json_list = []   
+        store_json_list = []
         for store_num in range(number_of_stores):
             response = requests.get(url.format(store_number=store_num), headers=headers)
             if response.status_code == 200:
@@ -50,38 +47,56 @@ class DataExtractor:
         return store_df
 
 
-
-
 # Milestone 2.3
 aws_connector = DatabaseConnector()
 extractor = DataExtractor()
-user_df = extractor.read_rds_table(aws_connector, 'legacy_users', 'db_creds.yaml')
+user_df = extractor.read_rds_table(aws_connector, "legacy_users", "db_creds.yaml")
 cleaner = DataCleaning()
-cleaner.clean_user_data(user_df, index_col='index')
+cleaner.clean_user_data(user_df, index_col="index")
 
 local_connector = DatabaseConnector()
-creds1 = local_connector.read_db_creds('db_creds_local.yaml')
+creds1 = local_connector.read_db_creds("db_creds_local.yaml")
 engine = local_connector.init_db_engine(creds1)
-local_connector.upload_to_db(user_df, 'dim_users')
+local_connector.upload_to_db(user_df, "dim_users")
 
 # Milestone 2.4
-card_df = DataExtractor.retrieve_pdf_data("https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf")
+card_df = DataExtractor.retrieve_pdf_data(
+    "https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf"
+)
 cleaner.clean_card_data(card_df)
-local_connector.upload_to_db(card_df, 'dim_card_details')
+local_connector.upload_to_db(card_df, "dim_card_details")
 
 # Milestone 2.5
 store_api_data = {
-    'endpoints': {
-        'number_stores':'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores', 
-        'store_details':'https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/{store_number}'
+    "endpoints": {
+        "number_stores": "https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores",
+        "store_details": "https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/store_details/{store_number}",
     },
     'headers': {
         'x-api-key': 'yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX'
     }
 }
 
-number_of_stores = extractor.list_number_of_stores(store_api_data['endpoints']['number_stores'], store_api_data['headers'])
-store_df = extractor.retrieve_stores_data(store_api_data['endpoints']['store_details'], store_api_data['headers'])
+number_of_stores = extractor.list_number_of_stores(
+    store_api_data["endpoints"]["number_stores"], store_api_data["headers"]
+)
+store_df = extractor.retrieve_stores_data(
+    store_api_data["endpoints"]["store_details"], store_api_data["headers"]
+)
 cleaner.clean_store_data(store_df)
-store_df = store_df.reindex(columns=['index', 'store_code', 'store_type', 'staff_numbers', 'address', 'longitude',	'latitude', 'locality', 'country_code', 'continent', 'opening_date'])
+store_df = store_df.reindex(
+    columns=[
+        "index",
+        "store_code",
+        "store_type",
+        "staff_numbers",
+        "address",
+        "longitude",
+        "latitude",
+        "locality",
+        "country_code",
+        "continent",
+        "opening_date",
+    ]
+)
 store_df
